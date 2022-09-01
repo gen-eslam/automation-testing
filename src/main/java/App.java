d231d641bcdf52eb96759cd6152bec3460c082c3
GEN_eslam abdelazize mohamed <78043455+gen-eslam@users.noreply.github.com>
2022-09-01 21:00:09 +0200

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class App {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome,driver","chromedriver.exe");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://www.google.com");
+        driver.close();
+    }
+}
